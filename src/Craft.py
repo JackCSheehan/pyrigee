470921c82122b78bdd39e5c9e5286f839aca99f6
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-11-02 01:53:18 -0600

    Animated dummy craft around body

--- src/Craft.py
+++ src/Craft.py
@@ -7,9 +7,10 @@ Class used for defining spacecraft to orbit a body
 '''
 class Craft:
     '''
-    Init function takes craft's mass (in kg) and the craft's color
+    Init function takes craft's name, mass (in kg) and the craft's color
     '''
-    def __init__(self, m, c):
+    def __init__(self, n, m, c):
+        self.name = n
         self.mass = m
         self.color = c
 
@@ -20,24 +21,24 @@ Sample Craft constants representing some famous crafts
 '''
 NASA's Space Shuttle. Mass based on shuttle wet mass
 '''
-SPACE_SHUTTLE = Craft(110000, "k")
+SPACE_SHUTTLE = Craft("Space Shuttle", 110000, "k")
 
 '''
 The first man-made object in orbit, the Soviet Union's Sputnik 1
 '''
-SPUTNIK_1 = Craft(84, "dimgray")
+SPUTNIK_1 = Craft("Sputnik 1", 84, "dimgray")
 
 '''
 The International Space Station
 '''
-ISS = Craft(420000, "orangered")
+ISS = Craft("ISS", 420000, "orangered")
 
 '''
 The Soviet Union's/Russia's Mir space station
 '''
-MIR = Craft(130000, "firebrick")
+MIR = Craft("Mir", 130000, "firebrick")
 
 '''
 NASA's Skylab space station. Mass not counting Apollo crew spacecraft
 '''
-SKYLAB = Craft(77000, "thistle")
+SKYLAB = Craft("Skylab", 77000, "thistle")
