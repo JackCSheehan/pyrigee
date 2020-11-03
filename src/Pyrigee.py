9f8d6c017c56dcc087a19b30a79019ae1398f279
Jack Sheehan <jaxanimations@gmail.com>
2020-11-03 02:21:57 -0600

    Changed orbit drawing algorithm to use polar form of ellipse

--- src/Pyrigee.py
+++ src/Pyrigee.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 
 # The number of divisions in wireframe plots or line plots
-DIVS = 12j
+DIVS = 15j
 
 # The number of km that each tick represents
 TICK_VALUE = 1000
@@ -71,6 +71,7 @@ def __animate_craft(frame, x, y, z, craft_point, craft_text):
     craft_text.set_position(np.append(x[frame], y[frame]))
     craft_text.set_3d_properties(z[frame], None)
 
+'''
 def plot(body, orbit, craft):
 
     # Standard matplotlib initialization items
@@ -99,7 +100,7 @@ def plot(body, orbit, craft):
     __plot_body(body, ax)
 
     # Calculate major axis based on given apogee/perigee and the radius of the body
-    major_axis = ((orbit.apogee / 2) + (orbit.perigee / 2 ) + body.radius)
+    major_axis = orbit.apogee + orbit.perigee + body.radius
     
     # Calculate semi-major axis based on calculated major axis
     semi_major_axis = major_axis / 2
@@ -109,6 +110,9 @@ def plot(body, orbit, craft):
 
     # Calculate distance between the center and a focus
     center_to_focus_distance = abs(semi_major_axis - side_to_focus_distance)
+
+    # Calculate the orbit's eccentricity. Used to graph a circle, ellipse, hyperbola, or parabola
+    eccentricity = center_to_focus_distance / side_to_focus_distance
 
     # Part of the equation to find minor axis. Equal to -F^2, the distance from center to focus squared * -1
     big_f = -(center_to_focus_distance**2)
@@ -127,14 +131,15 @@ def plot(body, orbit, craft):
     major_axis /= TICK_VALUE
     minor_axis /= TICK_VALUE
 
+    print(eccentricity)
+    print(math.sqrt(1 - ((semi_major_axis**2) / (semi_minor_axis**2))))
+
     # Calculate x and y points of the orbit ellipse
-    x = major_axis * np.cos(np.linspace(0, 2 * np.pi))
-    y = minor_axis * np.sin(np.linspace(0, 2 * np.pi))
+    x = (semi_major_axis / TICK_VALUE) * np.cos(np.linspace(0, 2 * np.pi))
+    y = (semi_minor_axis / TICK_VALUE) * np.sin(np.linspace(0, 2 * np.pi))
     #z = x * np.tan(np.radians(orbit.inclination))
 
     #print(z)
-    print(center_to_focus_distance / side_to_focus_distance)
-    print(side_to_focus_distance)
 
     orbit_offset = 0
 
@@ -160,7 +165,65 @@ def plot(body, orbit, craft):
     # Animate spacecraft. Must be assigned to a variable to work
     #a = animation.FuncAnimation(fig, __animate_craft, frames = x.size, fargs = (x + orbit_offset, y, x, craft_point, craft_text), blit = False, interval = ANIMATION_INTERVAL, repeat = True)
 
-    #ax.view_init(azim = 0, elev = 90)
+    ax.view_init(azim = 0, elev = 90)
 
     # Show the matplotlib window
+    plt.show()
+    '''
+
+def plot(body, orbit, craft):
+
+    # Standard matplotlib initialization items
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111, projection = "3d", proj_type = "ortho")
+    
+    # Set background colors to black
+    fig.patch.set_facecolor("k")
+    ax.set_facecolor("k")
+
+    # Change axis colors to white
+    ax.xaxis.label.set_color("white")
+    ax.yaxis.label.set_color("white")
+    ax.zaxis.label.set_color("white")
+    ax.tick_params(axis = "x", colors = "white")
+    ax.tick_params(axis = "y", colors = "white")
+    ax.tick_params(axis = "z", colors = "white")
+    
+    # Change grid fill to black and grid lines to white
+    ax.w_xaxis.set_pane_color((0, 0, 0, 0))
+    ax.w_yaxis.set_pane_color((0, 0, 0, 0))
+    ax.w_zaxis.set_pane_color((0, 0, 0, 0))
+    ax.grid(color = "red")
+
+    # Plot the given body
+    __plot_body(body, ax)
+
+    # Calculate major axis by adding apogee, perigee, and body diamter
+    major_axis = orbit.apogee + orbit.perigee + (2 * body.radius)
+
+    # Calculate semi-major axis from major axi
+    semi_major_axis = major_axis / 2
+
+    # Scale the apogee and perigee so that it is relative to the body itself rather than (0, 0)
+    scaled_apogee = orbit.apogee + body.radius
+    scaled_perigee = orbit.perigee + body.radius
+
+    # Calculate eccentricity of scaled orbit
+    scaled_eccentricity = (scaled_apogee - scaled_perigee) / (scaled_apogee + scaled_perigee)
+
+    # Polar equation of ellipse. Uses scaled eccentricity to draw orbit at correct size
+    r = (semi_major_axis * (1 - scaled_eccentricity**2)) / (1 - scaled_eccentricity * np.cos(np.linspace(0, 2 * np.pi)))
+
+    # Convert polar equations to cartesean coords
+    x = r * np.cos(np.linspace(0, 2 * np.pi))
+    y = r * np.sin(np.linspace(0, 2 * np.pi))
+
+    # Plot the orbit after scaling x and y coords to display in the correct units on graph
+    orbit_plot = ax.plot(x / TICK_VALUE, y / TICK_VALUE, zs = 0, zdir = "z", color = craft.color)
+
+    ax.view_init(azim = 0, elev = 90)
+
+    # Show the matplotlib window after setting layout to tight to ensure subplot takes up as much of fig as possible
+    plt.tight_layout()
     plt.show()
