177f3c8bf467fb3681aa4d25fef1f395653e9775
Jack Sheehan <jaxanimations@gmail.com>
2020-11-16 23:48:46 -0600

    Moved position of inclination arrow

--- src/main.py
+++ src/main.py
@@ -12,14 +12,14 @@ body = EARTH
 
 craft = SPACE_SHUTTLE
 
-orbit1 = Orbit(100000, 100, 25)
-orbit2 = Orbit(400, 400, 0)
+orbit1 = Orbit(100, 100, 0)
+orbit2 = Orbit(40000, 40000, 25)
 
 man = Maneuver("Station Transfer", orbit2, ManeuverType.HOHMANN_TRANSFER_ORBIT, "lightseagreen")
 
 p = Pyrigee()
 
-p.plot(body, orbit1, craft)
+p.plot(body, orbit1, craft, man)
 
 p.visualize()
 
