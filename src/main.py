45fa01181a49a9f2b82b3d42ed41bb9997dcb75a
Jack Sheehan <jaxanimations@gmail.com>
2020-11-27 16:35:05 -0600

    Cleaned up repetitive code for scaling orbit coordinates

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ body = EARTH
 
 craft = SPACE_SHUTTLE
 
-orbit2 = Orbit(1000000000000000000000000, 100, 0)
+orbit2 = Orbit(10000000000000000000000000, 100, 45)
 #orbit1 = Orbit(40000, 40000, 25)
 
 #man = Maneuver(orbit2, "lightseagreen")
