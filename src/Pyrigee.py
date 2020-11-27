5f91e76f96f264440a79c6ff03e06b7e1c405f8e
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-11-27 16:45:26 -0600

    Made Pyrigee class handle all imports

---
+++ src/Pyrigee.py
@@ -0,0 +1,8 @@
+'''
+Main file for the Pyrigee package that handles importing of relevant files
+'''
+from orbit_plotter import *
+from body import *
+from craft import *
+from orbit import *
+from maneuver import *
