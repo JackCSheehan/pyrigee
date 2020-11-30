9501720ad7e93c736444c8e879118cd2bac53a62
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-11-30 16:18:35 -0600

    Reduced z calc for elliptical orbits to smaller equation

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ body = Body("Earth", 5.9722e24, 6378, "cornflowerblue")
 craft = Craft("GOES-16", "chartreuse")
 
 # Create orbits
-initial_orbit = Orbit(10, 10, 50)
-target_orbit = Orbit(10000, 10000, 0)
+initial_orbit = Orbit(400, 400, 80)
+#target_orbit = Orbit(10, 10, 50)
 
-maneuver = Maneuver(target_orbit, "red")
+#maneuver = Maneuver(target_orbit, "red")
 
 p = OrbitPlotter(body)
 
 # When plotting, pass maneuver as third parameter
-p.plot(initial_orbit, craft, maneuver)
+p.plot(initial_orbit, craft)
 p.visualize()
