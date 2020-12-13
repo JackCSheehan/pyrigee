c4921eca413405577856edcf0182c2cb57df2bda
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-12-13 13:21:00 -0600

    Added __init__.py and setup.py files in preparation for uploading to PyPi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jscs",
+    version="0.1",
+    author="Jack Sheehan",
+    description="A python package for visualizing spacecraft orbits and orbital maneuvers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JackCSheehan/pyrigee",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires='>=3.6',
+)
