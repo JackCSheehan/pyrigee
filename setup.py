3fa9e776261f49de2c028af8fb496fb9c871c6ad
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-12-14 17:51:22 -0600

    Changed version number for development of future patches

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrigee",
-    version="1.0.3",
+    version="1.0.4",
     author="Jack Sheehan",
     description="A python package for visualizing spacecraft orbits and orbital maneuvers.",
     long_description=long_description,
